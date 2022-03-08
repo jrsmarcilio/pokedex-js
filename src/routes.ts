15826e55ba4ff156665ec8b1ca651092c7647c3d
Marcilio Júnior <jrsmarcilio@gmail.com>
2022-03-08 15:59:48 -0300

    first commit

--- src/routes.ts
+++ src/routes.ts
@@ -7,7 +7,7 @@ const createPokemonController = new CreatePokemonController();
 const routes = Router();
 
 routes.post("/pokemon", createPokemonController.handle);
-routes.post("/", (req: Request, res: Response) =>
+routes.get("/", (req: Request, res: Response) =>
   res.sendStatus(200).json({ application: "running" })
 );
 
